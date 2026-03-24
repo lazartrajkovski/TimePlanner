ab835e799c8c78343eac75c9409586a325e05604
lazartrajkovski <lazar.trajkovski@hotmail.com>
2026-03-24 22:22:05 +0100

    Add DbContext and EF Core configuration

---
+++ TimePlanner.Repositories/Data/TimePlannerDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TimePlanner.Domain.Entities;
+
+namespace TimePlanner.Repositories.Data
+{
+    public class TimePlannerDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Assignment> Assignments { get; set; }
+        public TimePlannerDbContext(DbContextOptions<TimePlannerDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ TimePlanner.Repositories/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TimePlanner.Repositories.Data;
+
+namespace TimePlanner.Repositories
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddRepositories(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<TimePlannerDbContext>(options =>
+                options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
+            return services;
+        }
+    }
+}
