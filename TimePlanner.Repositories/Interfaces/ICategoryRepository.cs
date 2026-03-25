4b6e562ce69511ef49c2b1615c8323aa1536cdbc
lazartrajkovski <lazar.trajkovski@hotmail.com>
2026-03-25 10:32:29 +0100

    Add Repository interfaces

---
+++ TimePlanner.Repositories/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using TimePlanner.Domain.Entities;
+
+namespace TimePlanner.Repositories.Interfaces
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+
+    }
+}
---
+++ TimePlanner.Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,8 @@
+﻿using TimePlanner.Domain.Entities;
+
+namespace TimePlanner.Repositories.Interfaces
+{
+    public interface IUserRepository : IRepository<User>
+    {
+    }
+}
